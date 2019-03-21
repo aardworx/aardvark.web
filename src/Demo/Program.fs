b55a9f01091d7a438b6627a83971111457d35474
krauthaufen <krauthaufen@awx.at>
2019-03-21 18:18:30 +0100

    import

---
+++ src/Demo/Program.fs
@@ -0,0 +1,14 @@
+﻿module Program
+
+open Fable.Core
+open Fable.Import.Browser
+
+
+[<EntryPoint>]
+let main argv =
+
+    
+    document.write "<h1>YEAH</h1>"
+
+    console.warn "sepp"
+    0 // return an integer exit code
