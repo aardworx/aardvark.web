7aea90f37813a104f897bc9e3a5ea768f70ba4f0
krauthaufen <krauthaufen@awx.at>
2019-04-24 21:02:21 +0200

    sync

--- build.cmd
+++ build.cmd
@@ -3,18 +3,27 @@ setlocal enableextensions enabledelayedexpansion
 PUSHD %~dp0
 
 if NOT exist .paket\Paket.Restore.targets (
-	powershell write-host -fore Green paket restore
+	powershell write-host -fore Red paket restore
 	.paket\paket.exe restore
+	if errorlevel 1 (
+      exit /b %errorlevel%
+    )
 )
 
 if NOT exist node_modules (
-	powershell write-host -fore Green yarn install
-	yarn install
+	powershell write-host -fore Red yarn install
+	cmd.exe /c "yarn install"
+	REM if errorlevel 1 (
+      REM exit /b %errorlevel%
+    REM )
 )
 
 if NOT exist paket-files\krauthaufen\Fable\build\fable-library (
-	powershell write-host -fore Green build compiler
+	powershell write-host -fore Red build compiler
 	yarn build-compiler
+	if errorlevel 1 (
+      exit /b %errorlevel%
+    )
 )
 
 yarn %*
