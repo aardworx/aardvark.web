74c16f102e9f63db0c01be24284edcd733431cb6
krauthaufen <krauthaufen@awx.at>
2019-04-24 21:09:13 +0200

    sync

--- build.cmd
+++ build.cmd
@@ -13,17 +13,11 @@ if NOT exist .paket\Paket.Restore.targets (
 if NOT exist node_modules (
 	powershell write-host -fore Red yarn install
 	cmd.exe /c "yarn install"
-	REM if errorlevel 1 (
-      REM exit /b %errorlevel%
-    REM )
 )
 
 if NOT exist paket-files\krauthaufen\Fable\build\fable-library (
 	powershell write-host -fore Red build compiler
-	yarn build-compiler
-	if errorlevel 1 (
-      exit /b %errorlevel%
-    )
+	cmd.exe /c "yarn build-compiler"
 )
 
-yarn %*
+cmd.exe /c "yarn %*"
