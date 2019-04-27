80e086a3a23fe12383eb34de4f094919a0004eff
krauthaufen <krauthaufen@awx.at>
2019-04-27 20:19:11 +0200

    linux build script

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -f .paket/Paket.Restore.targets ]; then
+if [ ! -f .paket/Paket.Restore.targets ]; then
     mono .paket/paket.exe restore
 fi
 
-if [ -f node_modules ]; then
+if [ ! -f node_modules ]; then
     npm install
 fi
 
-if [ -f paket-files/fable-compiler/Fable/build/fable-library ]; then
+if [ ! -f paket-files/fable-compiler/Fable/build/fable-library ]; then
 	npm run build-compiler
 fi
 
