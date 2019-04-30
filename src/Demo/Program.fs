a3afc671657843a6d950648737f12c5185b83770
krauthaufen <krauthaufen@awx.at>
2019-04-30 17:10:01 +0200

    added AMap

--- src/Demo/Program.fs
+++ src/Demo/Program.fs
@@ -7,6 +7,12 @@ open Aardvark.SceneGraph
 open Aardvark.Rendering.WebGL
 open Aardvark.Import.Browser
 
+type RenderCommand =
+    | Render of aset<RenderObject>
+    | Clear of colors : amap<string, V4d> * depth : IMod<Option<float>> * stencil : IMod<Option<int>>
+    | Unordered of aset<RenderCommand>
+
+
 module FShadeTest =
     open FShade
     type Vertex = 
@@ -73,136 +79,136 @@ module FShadeTest =
 [<EntryPoint>]
 let main argv =
 
-    FShade.SimpleSample.run()
 
-    //document.addEventListener_readystatechange(fun e ->
-    //    if document.readyState = "complete" then
 
-    //        //console.error (typeof<int[]>.GetElementType())
+    document.addEventListener_readystatechange(fun e ->
+        if document.readyState = "complete" then
 
-    //        let canvas = document.createElement_canvas()
-    //        canvas.tabIndex <- 1.0
-    //        document.body.appendChild(canvas) |> ignore
-    //        document.body.style.margin <- "0"
-    //        document.body.style.padding <- "0"
-    //        canvas.style.width <- "100%"
-    //        canvas.style.height <- "100%"
+            //console.error (typeof<int[]>.GetElementType())
+
+            let canvas = document.createElement_canvas()
+            canvas.tabIndex <- 1.0
+            document.body.appendChild(canvas) |> ignore
+            document.body.style.margin <- "0"
+            document.body.style.padding <- "0"
+            canvas.style.width <- "100%"
+            canvas.style.height <- "100%"
             
-    //        let control = new Aardvark.Application.RenderControl(canvas, true)
+            let control = new Aardvark.Application.RenderControl(canvas, true)
 
-    //        let initial = CameraView.lookAt (V3d(6.0, 6.0, 4.0)) V3d.Zero V3d.OOI
-    //        let cam = Aardvark.Application.DefaultCameraController.control control.Mouse control.Keyboard control.Time initial
-    //        let anim = Mod.constant true //control.Keyboard.IsDown(Aardvark.Application.Keys.Space)
-    //        let angle =
-    //            Mod.integrate 0.0 control.Time [
-    //                anim |> Mod.map (fun a ->
-    //                    if a then 
-    //                        control.Time |> Mod.stepTime (fun _ dt o -> o + 0.1 * dt)
-    //                    else
-    //                        AFun.create id
-    //                )
-    //            ]
-
-
-    //        let view = cam |> Mod.map CameraView.viewTrafo
-    //        let proj = control.Size |> Mod.map (fun s ->  Frustum.perspective 70.0 0.1 1000.0 (float s.X / float s.Y) |> Frustum.projTrafo)
+            let initial = CameraView.lookAt (V3d(6.0, 6.0, 4.0)) V3d.Zero V3d.OOI
+            let cam = Aardvark.Application.DefaultCameraController.control control.Mouse control.Keyboard control.Time initial
+            let anim = Mod.constant true //control.Keyboard.IsDown(Aardvark.Application.Keys.Space)
+            let angle =
+                Mod.integrate 0.0 control.Time [
+                    anim |> Mod.map (fun a ->
+                        if a then 
+                            control.Time |> Mod.stepTime (fun _ dt o -> o + 0.1 * dt)
+                        else
+                            AFun.create id
+                    )
+                ]
 
 
+            let view = cam |> Mod.map CameraView.viewTrafo
+            let proj = control.Size |> Mod.map (fun s ->  Frustum.perspective 70.0 0.1 1000.0 (float s.X / float s.Y) |> Frustum.projTrafo)
 
 
-    //        let sphere =
-    //            Sg.sphere 2
-    //            |> Sg.trafo (Mod.constant (Trafo3d.Scale(0.5)))
 
-    //            |> Sg.viewTrafo view
-    //            |> Sg.projTrafo proj
-    //            |> Sg.uniform "DiffuseColorTexture" (Mod.constant (FileTexture "pattern.jpg" :> ITexture))
+
+            let sphere =
+                Sg.sphere 2
+                |> Sg.trafo (Mod.constant (Trafo3d.Scale(0.5)))
+
+                |> Sg.viewTrafo view
+                |> Sg.projTrafo proj
+                |> Sg.uniform "DiffuseColorTexture" (Mod.constant (FileTexture "pattern.jpg" :> ITexture))
                 
-    //        let sphere2 =
-    //            Sg.sphere 4
-    //            |> Sg.trafo (Mod.constant (Trafo3d.Scale(0.5) ))
+            let sphere2 =
+                Sg.sphere 4
+                |> Sg.trafo (Mod.constant (Trafo3d.Scale(0.5) ))
 
-    //            |> Sg.viewTrafo view
-    //            |> Sg.projTrafo proj
-    //            |> Sg.uniform "DiffuseColorTexture" (Mod.constant (FileTexture "cliffs_color.jpg" :> ITexture))
+                |> Sg.viewTrafo view
+                |> Sg.projTrafo proj
+                |> Sg.uniform "DiffuseColorTexture" (Mod.constant (FileTexture "cliffs_color.jpg" :> ITexture))
                     
-    //        let box =
-    //            Sg.box Box3d.Unit
-    //            |> Sg.trafo (Mod.constant (Trafo3d.Translation(-0.5, -0.5, -0.5)))
+            let box =
+                Sg.box Box3d.Unit
+                |> Sg.trafo (Mod.constant (Trafo3d.Translation(-0.5, -0.5, -0.5)))
 
-    //            |> Sg.viewTrafo view
-    //            |> Sg.projTrafo proj
-    //            |> Sg.uniform "DiffuseColorTexture" (Mod.constant (FileTexture "test.jpg" :> ITexture))
+                |> Sg.viewTrafo view
+                |> Sg.projTrafo proj
+                |> Sg.uniform "DiffuseColorTexture" (Mod.constant (FileTexture "test.jpg" :> ITexture))
 
-    //        let rand = System.Random()
-    //        let sett =
-    //            let s = 2
-    //            cset [
-    //                for x in -s/2 .. (s - s/2 - 1) do
-    //                    for y in -s/2 .. (s - s/2 - 1) do
-    //                        for z in -s/2 .. (s - s/2 - 1) do
-    //                            let r = rand.NextDouble()
+            let rand = System.Random()
+            let sett =
+                let s = 2
+                cset [
+                    for x in -s/2 .. (s - s/2 - 1) do
+                        for y in -s/2 .. (s - s/2 - 1) do
+                            for z in -s/2 .. (s - s/2 - 1) do
+                                let r = rand.NextDouble()
                                 
-    //                            let sg =
-    //                                if r > 0.6 then sphere2
-    //                                elif r > 0.3 then sphere
-    //                                else box
+                                let sg =
+                                    if r > 0.6 then sphere2
+                                    elif r > 0.3 then sphere
+                                    else box
 
-    //                            let t = V3d(float x, float y, float z) * 1.5
-    //                            let axis = V3d(rand.NextDouble() * 20.0 - 10.0, rand.NextDouble() * 20.0 - 10.0, rand.NextDouble() * 20.0 - 10.0)|> Vec.normalize
+                                let t = V3d(float x, float y, float z) * 1.5
+                                let axis = V3d(rand.NextDouble() * 20.0 - 10.0, rand.NextDouble() * 20.0 - 10.0, rand.NextDouble() * 20.0 - 10.0)|> Vec.normalize
 
-    //                            let speed = rand.NextDouble() * 40.0 - 20.0
-    //                            let model = angle |> Mod.map (fun a -> Trafo3d.Rotation(axis, speed * a))
-    //                            let s = 
-    //                                sg 
-    //                                |> Sg.trafo model
-    //                                |> Sg.trafo (Mod.constant <| Trafo3d.Translation t)
-    //                            yield s
-    //            ]
+                                let speed = rand.NextDouble() * 40.0 - 20.0
+                                let model = angle |> Mod.map (fun a -> Trafo3d.Rotation(axis, speed * a))
+                                let s = 
+                                    sg 
+                                    |> Sg.trafo model
+                                    |> Sg.trafo (Mod.constant <| Trafo3d.Translation t)
+                                yield s
+                ]
 
-    //        let mode = Mod.init (FShadeTest.A { FShadeTest.ambient = 0.1; FShadeTest.diffuse = 0.9 })
+            let mode = Mod.init (FShadeTest.A { FShadeTest.ambient = 0.1; FShadeTest.diffuse = 0.9 })
 
-    //        let sg =
-    //            Sg.set sett
-    //            |> Sg.effect [
-    //                FShade.Effect.ofFunction (FShadeTest.constantColor V4d.IOOI)
-    //                FShade.Effect.ofFunction FShadeTest.trafo
-    //                FShade.Effect.ofFunction FShadeTest.diffuseTexture
-    //                FShade.Effect.ofFunction FShadeTest.simpleLight
-    //            ]
-    //            |> Sg.uniform "Ambient" mode
+            let sg =
+                Sg.set sett
+                |> Sg.effect [
+                    FShade.Effect.ofFunction (FShadeTest.constantColor V4d.IOOI)
+                    FShade.Effect.ofFunction FShadeTest.trafo
+                    FShade.Effect.ofFunction FShadeTest.diffuseTexture
+                    FShade.Effect.ofFunction FShadeTest.simpleLight
+                ]
+                |> Sg.uniform "Ambient" mode
 
 
-    //        let objects = sg.RenderObjects()
-    //        let task() = new RenderTask(control.FramebufferSignature, control.Manager, objects) :> IRenderTask
+            let objects = sg.RenderObjects()
+            let task() = new RenderTask(control.FramebufferSignature, control.Manager, objects) :> IRenderTask
 
-    //        let mutable active = true
-    //        control.Keyboard.DownWithRepeats.Add(fun k ->
-    //            match k with
-    //            | Aardvark.Application.Keys.Space ->
-    //                transact (fun () ->
-    //                    match mode.Value with
-    //                    | FShadeTest.A _ -> mode.Value <- FShadeTest.B 1.2
-    //                    | FShadeTest.B _ -> mode.Value <- FShadeTest.A { FShadeTest.ambient = 0.1; FShadeTest.diffuse = 0.9 }
-    //                )
-    //            | Aardvark.Application.Keys.Delete ->
-    //                if active then
-    //                    active <- false
-    //                    control.RenderTask <- RenderTask.empty
-    //                else
-    //                    active <- true
-    //                    control.RenderTask <- task()
-    //            | Aardvark.Application.Keys.X ->
+            let mutable active = true
+            control.Keyboard.DownWithRepeats.Add(fun k ->
+                match k with
+                | Aardvark.Application.Keys.Space ->
+                    transact (fun () ->
+                        match mode.Value with
+                        | FShadeTest.A _ -> mode.Value <- FShadeTest.B 1.2
+                        | FShadeTest.B _ -> mode.Value <- FShadeTest.A { FShadeTest.ambient = 0.1; FShadeTest.diffuse = 0.9 }
+                    )
+                | Aardvark.Application.Keys.Delete ->
+                    if active then
+                        active <- false
+                        control.RenderTask <- RenderTask.empty
+                    else
+                        active <- true
+                        control.RenderTask <- task()
+                | Aardvark.Application.Keys.X ->
                     
-    //                if sett.Count > 0 then
-    //                    //transact (fun () -> sett.Clear())
-    //                    let e = sett |> Seq.item (rand.Next sett.Count)
-    //                    transact (fun () -> sett.Remove e |> ignore)
-    //            | _ ->
-    //                ()
-    //        )
+                    if sett.Count > 0 then
+                        //transact (fun () -> sett.Clear())
+                        let e = sett |> Seq.item (rand.Next sett.Count)
+                        transact (fun () -> sett.Remove e |> ignore)
+                | _ ->
+                    ()
+            )
 
 
-    //        control.RenderTask <- task()
-    //)
+            control.RenderTask <- task()
+    )
     0
