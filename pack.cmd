e4ee65e21e0eebfea28da2e19663ba1347d5219d
krauthaufen <krauthaufen@awx.at>
2019-06-06 13:04:35 +0200

    added push script

--- pack.cmd
+++ pack.cmd
@@ -9,15 +9,10 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 DEL tmpFile
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-set cont=y
-REM powershell Write-Host -NoNewline -ForegroundColor red "create packages  Version: %packageversion%? [Yn]" 
-REM if %errorlevel% neq 0 exit /b %errorlevel%
-REM set /P cont=""
+SET /p key= < %HOMEPATH%\.ssh\aardworx.key
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo %key%
 
-if /I "%cont%"=="n" (
-	powershell Write-Host -ForegroundColor red okay, bye
-) else (
-	powershell Write-Host -ForegroundColor red creating packages with version %packageversion%
-	dotnet pack /p:Version=%packageversion% /p:AssemblyVersion=%packageversion% /p:Authors=Aardworx /p:Copyright=Aardworx /p:RepositoryUrl=https://github.com/aardworx/aardvark.web /p:PackageLicenseExpression=AGPL-3.0-only -o %~dp0dist\ -c Release
-	if %errorlevel% neq 0 exit /b %errorlevel%
-)
+powershell Write-Host -ForegroundColor red creating packages with version %packageversion%
+REM dotnet pack /p:Version=%packageversion% /p:AssemblyVersion=%packageversion% /p:Authors=Aardworx /p:Copyright=Aardworx /p:RepositoryUrl=https://github.com/aardworx/aardvark.web /p:PackageLicenseExpression=AGPL-3.0-only -o %~dp0dist\ -c Release
+if %errorlevel% neq 0 exit /b %errorlevel%
